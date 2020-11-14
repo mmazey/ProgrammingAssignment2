ff08e68a5542d8384b4e7cacd5485ce8f2fb8521
mmazey <mmazey@fuse.net>
2020-11-14 15:17:28 -0500

    Programming Assingnment 2 FINAL

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions can used to calculate the inverse of a matrix. They 
+## demonstrate lexical scoping and the advantages of caching potentially 
+## time-consuming processes. Example of using these functions would be:
 
-## Write a short comment describing this function
+## A <- matrix(c(1, 2, 3, 4), nrow = 2, ncol = 2)
+## invA <- cacheSolve(makeCacheMatrix(A))
+## invA
+
+## In this example, invA is the inverse of matrix A. 
+## As a check, invA %*% A = identity matrix
+
+
+## This function creates a list of four functions that act on a matrix, 
+## This function creates a special "matrix" object that can cache its inverse. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL # i (inverse) initialized to NULL
+  set <- function(y){
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) i <- inverse 
+  getinv <- function() i
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes as input a special "matrix", which is an output from the makeCacheMatrix
+## function, and it checks to see if the inverse of that "matrix"has already been 
+## calculated (and not been changed), and then if not, calculates and retrieves it.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getinv()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data)
+  x$setinv(i)
+  i
 }
